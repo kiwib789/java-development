e2df5764dd7d7ef1025589c49d60a83236579980
Keely Lowder <keelybowman@icloud.com>
2024-09-26 10:09:50 -0400

    first commit

---
+++ workbook-1/com.pluralsight/src/main/java/com/pluralsight/shoppinglist/ShoppingList.java
@@ -0,0 +1,7 @@
+package com.pluralsight.shoppinglist;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("Shopping List\n Eggs\n Milk\n Bread\n Butter\n Creamer\n Water\n Cheese\n Tea\n Hot dogs\n Jelly ");
+    }
+}
